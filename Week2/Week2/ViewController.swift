830d2cfcb5c7d212b810fa133809e3f25e709741
KS <bubbleguy10@gmail.com>
2024-09-27 17:47:06 +0900

    Add Week2

---
+++ Week2/Week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week2
+//
+//  Created by 이건수 on 2024.09.27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
